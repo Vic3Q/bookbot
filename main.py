fdd0bdaee80263877fbef356d1e2fea84bdb9f0f
git-user <jakub.kluba@gmail.com>
2025-12-16 14:55:08 +0100

    corrected code

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ book_path = "~/workspace/github.com/Vic3Q/bookbot/books/frankenstein.txt"
 book_text = get_book_text(book_path)
 
 words = book_text.split()
-print("Found ", len(words), " total words")
+print("Found", len(words), "total words")
