76a5801f964ae378c5ecef57d6c50fb23eab48e0
git-user <jakub.kluba@gmail.com>
2025-12-16 17:25:09 +0100

    move function to stats.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import os
-
-def get_book_text(path):
-    path = os.path.expanduser(path)
-    with open(path, "r", encoding="utf-8") as file:
-        content = file.read()
-    return content
+from stats import get_book_text
 
 book_path = "~/workspace/github.com/Vic3Q/bookbot/books/frankenstein.txt"
 book_text = get_book_text(book_path)
---
+++ stats.py
@@ -0,0 +1,5 @@
+def get_book_text(path):
+    path = os.path.expanduser(path)
+    with open(path, "r", encoding="utf-8") as file:
+        content = file.read()
+    return content
