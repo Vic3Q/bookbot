cd1b7240778cb29de9f806ec0fbbf9cc5f4f38e0
git-user <jakub.kluba@gmail.com>
2025-12-16 13:34:41 +0100

    file read

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-get_book_text(path){
+import os
 
+def get_book_text(path):
+    path = os.path.expanduser(path)
+    with open(path, "r", encoding="utf-8") as file:
+        content = file.read()
+    return content
 
-}
+book_path = "~/workspace/github.com/Vic3Q/bookbot/books/frankenstein.txt"
+book_text = get_book_text(book_path)
+print(book_text)
