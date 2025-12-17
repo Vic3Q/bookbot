037367417a1937b85030e68da93898c84f03b732
git-user <jakub.kluba@gmail.com>
2025-12-17 19:50:05 +0100

    added if to ensure path is provided as argument

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sys
 from stats import get_book_text, count_distinct_letters, sorter
 
-print("Usage: python3 main.py <path_to_book>")
-
-sys.exit(1)
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 book_path = sys.argv[1]
 book_text = get_book_text(book_path)
