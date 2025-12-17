784225f38f598d5341f2d11237dcc3989f0080b2
git-user <jakub.kluba@gmail.com>
2025-12-17 19:43:23 +0100

    changed hardcoded book path to command line argument Added usage message and exit if no argument provided

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import sys
 from stats import get_book_text, count_distinct_letters, sorter
 
-book_path = "~/workspace/github.com/Vic3Q/bookbot/books/frankenstein.txt"
+print("Usage: python3 main.py <path_to_book>")
+
+sys.exit(1)
+
+book_path = sys.argv[1]
 book_text = get_book_text(book_path)
 
 words = book_text.split()
