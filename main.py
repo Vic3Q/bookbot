cbc72766229419b0987f15910ea7e79ee94c6770
git-user <jakub.kluba@gmail.com>
2025-12-17 15:07:48 +0100

    liczenie liter

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from stats import get_book_text
+from stats import get_book_text, count_distinct_letters
 
 book_path = "~/workspace/github.com/Vic3Q/bookbot/books/frankenstein.txt"
 book_text = get_book_text(book_path)
 
 words = book_text.split()
+litery = count_distinct_letters(book_text)
 print("Found", len(words), "total words")
+print(litery)
--- stats.py
+++ stats.py
@@ -1,7 +1,13 @@
 import os
+from collections import Counter
 
 def get_book_text(path):
     path = os.path.expanduser(path)
     with open(path, "r", encoding="utf-8") as file:
         content = file.read()
     return content
+
+def count_distinct_letters(text):
+    text = text.lower()
+    licznik = Counter(text)
+    return licznik
